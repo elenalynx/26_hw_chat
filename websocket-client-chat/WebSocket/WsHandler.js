4099e25f7ddf0d8484f0d9415a07118b20829e79
elenalynx <elnlynx@gmail.com>
2023-01-28 01:12:27 +0200

    fixed bug in Forma.js. Code is working, but thete will be refactoring

--- websocket-client-chat/WebSocket/WsHandler.js
+++ websocket-client-chat/WebSocket/WsHandler.js
@@ -14,5 +14,4 @@ export class WsHandler extends WebSocket {
         }
     }
 
-    send;
 }
--- websocket-client-chat/index.js
+++ websocket-client-chat/index.js
@@ -2,11 +2,61 @@ import {WsHandler} from "./WebSocket/WsHandler.js";
 import {Form} from "./Entities/Form.js";
 
 const url = 'ws://localhost:8080';
+const ws = new WebSocket(url);
+const form = document.querySelector('#formWs');
+const dialog = document.querySelector('#chat-container');
+let inputsFormData;
+let users = {};
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    inputsFormData = new FormData(form);
+
+    users = {
+        user: inputsFormData.get('user'),
+        message: inputsFormData.get('message'),
+        id: 1,
+    };
+
+    ws.send(JSON.stringify(users));
+}
+
+console.log(ws)
+
+
+// onclose
+// onerror
+ws.onopen = () => console.log('Connection open');
+
+
+ws.onmessage = ({data}) => {
+    console.log(data)
+    const userData = JSON.parse(data);
+
+    renderMessage(userData);
+
+}
+
+function renderMessage( { user, message, id } ) {
+
+    console.log(`User: ${user} Message: ${message}`);
+    // console.log(event);
+    // console.log(`User:${} Message: ${data}`);
+    // if (dialog.closest('ul')) {
+    const html = `
+    <li data-id="${id}">${user}: ${message}</li>`;
+
+    dialog.insertAdjacentHTML('afterbegin', html);
+    // }
+}
 
 // const ws = new WsHandler(url);
 // console.log(ws)
 
-const formId = '#formWs';
-console.log(formId)
-const formHandler = new Form(formId, url);
-console.log(formHandler)
+//для пробы другой реализации
+// const formId = '#formWs';
+// console.log(formId)
+// const formHandler = new Form(formId, url);
+// console.log(formHandler)
